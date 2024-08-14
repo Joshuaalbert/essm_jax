d4948f66886b0d657c4c6ec663deea6b13d50da9
joshuaalbert <albert@strw.leidenuniv.nl>
2024-08-15 01:36:57 +0300

    * bump to 1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='essm_jax',
-      version='1.0.0',
+      version='1.0.1',
       description='Extended State Spapce Model in JAX',
       long_description=long_description,
       long_description_content_type="text/markdown",
